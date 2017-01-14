4eed25a7116f90dab0710fd8af62c7713e9c8b75
Alex Rapley <rapley3@gmail.com>
2017-01-14 15:15:22 +1100

    Overhauled JS structure, added home page link to footer, added press enter to search functionality

--- app.js
+++ app.js
@@ -1,54 +1,65 @@
-function gotData(data) {
-    //Error test
-    if (data.error) {
-        alert(data.error.info);
-        return;
+var app = {};
+
+app.init = function() {
+  // Initialise search on click
+  $('#search').on('click', app.onSearch);
+
+	// Initialise search on enter
+	$("#input").keypress(function(e) {
+		if ((e.keyCode || e.which) == '13') {
+			$('#search').click();
+		}
+	});
+};
+
+app.onSearch = function() {
+  var input = $('#input').val();
+  app.url = "//en.wikipedia.org/w/api.php?action=opensearch&search=" + input + "&format=json&callback=?";
+
+  $.getJSON(app.url).then(app.gotData, app.gotError);
+  $('#input').val("");
+};
+
+app.gotData = function(data) {
+  //Error test
+  if (data.error) {
+    alert(data.error.info);
+    return;
+  }
+
+  //Render results
+  app.titles = data[1];
+  app.descriptions = data[2];
+  app.links = data[3];
+
+  var itemTemplate = $('#itemTemplate').html();
+
+  $('.output').empty();
+
+  for (var i = 0; i < app.titles.length; i++) {
+    app.title = app.titles[i];
+    app.description = app.descriptions[i];
+    app.link = app.links[i];
+
+    // If any item's description contains "may refer to", skip loop cycle
+    if (app.description.match(/may refer to/i)) {
+      continue;
+    }
+    // If any item has an empty description...
+    if (app.description === '') {
+      app.description = "The description is missing.";
     }
 
-    //Render results
-    var titles = data[1];
-    var descriptions = data[2];
-    var links = data[3];
+    app.item = $(itemTemplate);
+    app.item.find(".title").text(app.title + ": ");
+    app.item.find(".description").text(app.description);
+    app.item.find(".link").attr("href", app.link);
+    $('.output').append(app.item).hide().fadeIn();
+  }
+};
 
-    var itemTemplate = $('#itemTemplate').html();
+app.gotError = function(err) {
+  alert("Error");
+};
 
-    $('.output').empty();
-
-    for (var i = 0; i < titles.length; i++) {
-        title = titles[i];
-        description = descriptions[i];
-        //If any item contains "may refer to" skip loop cycle
-        if (description.match(/may refer to/i)) {
-            continue;
-        }
-
-        if (description === '') {
-            description = "The description is missing.";
-        }
-
-        link = links[i];
-
-        item = $(itemTemplate);
-
-        item.find(".title").text(title + ": ");
-        item.find(".description").text(description);
-        item.find(".link").attr("href", link);
-        $('.output').append(item).hide().fadeIn();
-    }
-}
-
-function gotError(err) {
-    alert("Error");
-}
-
-function onSearch() {
-    var input = $('#input').val();
-    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + input + "&format=json&callback=?";
-
-
-    $.getJSON(url).then(gotData, gotError);
-    $('#input').val("");
-}
-
-//Initialise page
-$('#search').on('click', onSearch);
+$(document).ready(app.init);
